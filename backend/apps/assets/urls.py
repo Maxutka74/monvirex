69e230c247fbc16acf9c57dd8c92a38d7d206b26
mr-roboto <mr-roboto@gmail.com>
2026-06-17 19:17:42 +0300

    feat(assets) add routing and urls

---
+++ backend/apps/assets/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from apps.assets.views import AssetListView, AssetDetailView, AssetKlinesView
+
+urlpatterns = [
+    path('assets/', AssetListView.as_view(), name='assets'),
+    path('assets/klines/<str:symbol>/<str:interval>/<int:limit>/', AssetKlinesView.as_view(), name='asset-klines'),
+    path('assets/<str:symbol>/', AssetDetailView.as_view(), name='asset-detail')
+]
