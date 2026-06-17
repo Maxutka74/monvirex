ef8bfb9a202598a52181c694ad10fc61ca7ef3b8
mr-roboto <mr-roboto@gmail.com>
2026-06-17 19:09:02 +0300

    feat(assets): add Asset model and Binance API client integration

--- backend/apps/assets/models.py
+++ backend/apps/assets/models.py
@@ -1,3 +1,16 @@
+import uuid
+
 from django.db import models
 
 # Create your models here.
+class Asset(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    symbol = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=50, db_index=True)
+    icon_url = models.URLField(max_length=250, blank=True)
+    current_price = models.DecimalField(max_digits=30, decimal_places=10)
+    price_change_24h = models.DecimalField(max_digits=10, decimal_places=2)
+    volume_24h = models.DecimalField(max_digits=40, decimal_places=8)
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
