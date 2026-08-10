1001aa29c4a8a46ad28c1b935905ce797391466b
Maxutka73 <gouynoran@gmail.com>
2026-08-10 22:47:40 +0300

    refactor: update my assets layout

--- frontend/src/pages/myassets/MyAssetsPage.tsx
+++ frontend/src/pages/myassets/MyAssetsPage.tsx
@@ -1,7 +1,8 @@
-import WalletOverviewCard from "../../widgets/myassets/WalletOverviewCard.tsx";
-import MarketsCard from "../../widgets/myassets/MarketsCard.tsx";
-import BestToBuyChart from "../../widgets/myassets/BestToBuyChart.tsx";
+import WalletOverviewCard from "../../widgets/myassets/wallet-transactions/wallet/WalletOverviewCard.tsx";
+import MarketsCard from "../../widgets/myassets/markets/MarketsCard.tsx";
+import BestToBuyCard from "../../widgets/myassets/best-to-buy/BestToBuyCard.tsx";
 import AIChart from "../../shared/ui/AIChart.tsx";
+import FastActionCard from "../../widgets/myassets/trading/FastActionCard.tsx";
 
 
 const MyAssetsPage = () => {
@@ -9,19 +10,14 @@ const MyAssetsPage = () => {
         <section className="w-full px-4 sm:px-6 lg:px-8 2xl:px-12">
             <div className="mx-auto max-w-[1700px]">
                 <div className="grid grid-cols-1 xl:grid-cols-12 gap-6">
-                    <div className="xl:col-span-8">
+                    <div className="xl:col-span-8 flex flex-col gap-6">
                         <WalletOverviewCard />
-                    </div>
-
-                    <div className='xl:col-span-4'>
-                        <BestToBuyChart />
-                    </div>
-
-                    <div className="xl:col-span-8">
                         <MarketsCard />
                     </div>
 
-                    <div className='xl:col-span-4'>
+                    <div className="xl:col-span-4 flex flex-col gap-3">
+                        <BestToBuyCard />
+                        <FastActionCard />
                         <AIChart />
                     </div>
                 </div>
