36f9f14a2c61388e0101f4ffd2c931627754d1cc
Maxutka73 <gouynoran@gmail.com>
2026-08-10 22:47:54 +0300

    improve: update AI chart layout

--- frontend/src/shared/ui/AIChart.tsx
+++ frontend/src/shared/ui/AIChart.tsx
@@ -4,14 +4,16 @@ import ai_robot from "../../assets/images/Ai_Decoration.svg"
 
 const AIChart = () => {
     return (
-        <div className='relative h-[250px] flex flex-col justify-center gap-2 rounded-[20px] text-white bg-linear-to-tr from-[#9F87FF] to-[#429EFF] p-5'>
-            <h3 className='w-[400px] text-[32px]'>Trade smarter with MONVIREX AI</h3>
-            <p className='w-[350px]'>Automate trades based on user-defined criteria, using AI algorithms</p>
-            <div className='w-full max-w-[150px] h-[54px] flex flex-row justify-center items-center gap-3 text-black bg-white rounded-full cursor-pointer'>
-                <button>Try Now</button>
-                <FiArrowUpRight size={18} />
+        <div className='relative h-[250px] flex flex-row rounded-[20px] text-white bg-linear-to-tr from-[#9F87FF] to-[#429EFF] p-5'>
+            <div className="relative z-10 w-full h-full lg:w-[55%] flex flex-col justify-center gap-4">
+                <h3 className='text-[28px] whitespace-nowrap'>Trade smarter with <br/> MONVIREX AI</h3>
+                <p className='xl:whitespace-nowrap'>Automate trades based on user-defined criteria,<br className="hidden sm:block" /> using AI algorithms</p>
+                <div className='w-full max-w-[150px] h-[54px] flex flex-row justify-center items-center gap-3 text-black bg-white rounded-full cursor-pointer'>
+                    <button className='cursor-pointer'>Try Now</button>
+                    <FiArrowUpRight size={18} />
+                </div>
             </div>
-            <img className='absolute right-0.5 top-3 w-[186px] h-[244px]' src={ai_robot} alt="Robot AI"/>
+            <img className="absolute top-3 xl:top-3 2xl:top-3 -right-6 w-[186px] h-[244px]" src={ai_robot} alt='Robot AI'/>
         </div>
     )
 }
