35c35f7866a478570940dc3528dab57f2357a19e
Maxutka73 <gouynoran@gmail.com>
2026-08-10 22:50:08 +0300

    fix: remove React StrictMode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './app/styles/index.css'
 import App from './App.tsx'
@@ -8,11 +7,9 @@ import {GoogleOAuthProvider} from "@react-oauth/google";
 const queryClient = new QueryClient()
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
       <QueryClientProvider client={queryClient}>
           <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
             <App />
           </GoogleOAuthProvider>
       </QueryClientProvider>
-  </StrictMode>,
 )
